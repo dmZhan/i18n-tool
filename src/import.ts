359842378f59d3ac0a7d83b2e4f9537fe9c9bb52
Allen <1130123899@qq.com>
2023-07-31 10:00:30 +0800

    feat: wip

---
+++ src/import.ts
@@ -0,0 +1,14 @@
+// TODO 导入excel，解析包含数据，与本地json比较，加入本地json
+export function importExcel() {
+
+}
+
+// TODO 比较数据
+export function compareData() {
+
+}
+
+// TODO display data
+export async function displayData() {
+  // const files = await fg()
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { join, resolve } from 'node:path'
 import { cwd } from 'node:process'
 import { read, set_fs, utils } from 'xlsx'
+import type { WorkBook, WritingOptions } from 'xlsx'
 import fg from 'fast-glob'
 import fs from 'fs-extra'
 
 const rootDir = resolve(__dirname, '..')
+
 export async function getServerSideProps() {
   set_fs(await import('node:fs')) // dynamically import 'fs' in `getServerSideProps`
   const wb = read(join(cwd(), 'examples', '1.xlsx'), {
@@ -34,10 +36,68 @@ export async function getServerSideProps() {
 // }
 export async function generateExcel() {
   const langsRoot = resolve(rootDir, 'langs')
-  const files = await fg(['*.json', '!zh_cn.json'], { cwd: langsRoot })
-  const arr = []
-  for (const file of files)
-    arr.push(await fs.readJson(file))
-
-  return arr
+  const files = await fg(['zh_cn.json'], { cwd: langsRoot })
+  // const ws = utils.json_to_sheet(Object.values(flatJson(await fs.readJson(join(langsRoot, files[0])))))
+  return [{ zh_cn: '中文' }, ...Object.values(flatJson(await fs.readJson(join(langsRoot, files[0])))).map(item => ({ zh_cn: item }))]
 }
+
+export async function kk() {
+  const s: any[] = await generateExcel()
+  const wopts: WritingOptions = {
+    bookType: 'xlsx',
+    bookSST: false,
+    type: 'file',
+  }
+  const workBook: WorkBook = {
+    SheetNames: ['Sheet1'],
+    Sheets: {},
+    Props: {},
+  }
+  workBook.Sheets.Sheet1 = utils.json_to_sheet(s, { header: ['zh_cn'], skipHeader: true })
+  // writeFile(workBook, join(cwd(), 'examples', '2.xlsx'), wopts)
+  return workBook
+}
+export function flatJson(obj: Record<string, any>, pKey?: string): Record<string, string> {
+  let result: Record<string, string> = {}
+  Object.keys(obj).forEach((key) => {
+    const value = obj[key]
+    const _key = pKey ? `${pKey}.${key}` : key
+    if (typeof value === 'object')
+      result = { ...result, ...flatJson(value, _key) }
+
+    else
+      result[_key] = value
+  })
+
+  return result
+}
+
+// (async () => {
+//   /* fetch JSON data and parse */
+//   const url = 'https://theunitedstates.io/congress-legislators/executive.json'
+//   const raw_data = (await axios(url, { responseType: 'json' })).data
+
+//   /* filter for the Presidents */
+//   const prez = raw_data.filter(row => row.terms.some(term => term.type === 'prez'))
+
+//   /* flatten objects */
+//   const rows = prez.map(row => ({
+//     name: `${row.name.first} ${row.name.last}`,
+//     birthday: row.bio.birthday,
+//   }))
+
+//   /* generate worksheet and workbook */
+//   const worksheet = utils.json_to_sheet(rows)
+//   const workbook = utils.book_new()
+//   utils.book_append_sheet(workbook, worksheet, 'Dates')
+
+//   /* fix headers */
+//   utils.sheet_add_aoa(worksheet, [['Name', 'Birthday']], { origin: 'A1' })
+
+//   /* calculate column width */
+//   const max_width = rows.reduce((w, r) => Math.max(w, r.name.length), 10)
+//   worksheet['!cols'] = [{ wch: max_width }]
+
+//   /* create an XLSX file and try to save to Presidents.xlsx */
+//   writeFile(workbook, 'Presidents.xlsx')
+// })()
